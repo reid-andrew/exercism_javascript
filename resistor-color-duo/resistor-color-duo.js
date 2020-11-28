2dd803a7a12e0b451cb92374566f2c2b67b87cb7
Andrew Reid <areid.sj@gmail.com>
2020-11-28 10:25:28 -0700

    Pass all resistor duo tests

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,8 +1,14 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+var colors = {
+  "black": 0,
+  "brown": 1,
+  "red": 2,
+  "orange": 3,
+  "yellow": 4,
+  "green": 5,
+  "blue": 6,
+  "violet": 7,
+  "grey": 8,
+  "white": 9
+}
 
-export const decodedValue = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const decodedValue = (input) => colors[input[0]] * 10 + colors[input[1]];
